2dfd5f4937382db891bb5a6c6a48c26be8e88438
Yogga Aditya Candra <yogga910@gmail.com>
2025-10-06 11:36:46 +0700

    testing deploy lagi

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ class QueryRequest(BaseModel):
 
 # Inisialisasi aplikasi FastAPI dengan dokumentasi lengkap
 app = FastAPI(
-    title="🤖 NL-to-SQL Service API",
+    title="🤖 NL-to-SQL Service API -  Testing Deploy",
     description="""
     ## API Cerdas untuk Mengkonversi Bahasa Natural ke Query SQL
     
